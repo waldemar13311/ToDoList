5ad270984c0ac2ebb97bce2f01370e1d0a3d1179
waldemar13311 <waldemar13311@gmail.com>
2021-11-01 18:01:46 +0300

    Forget files

---
+++ Models/SignInUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class SignInUserModel
+    {
+        [Required(ErrorMessage = "Login is not specified")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password is not specified")]
+        [UIHint("Password")]
+        public string Password { get; set; }
+    }
+}
