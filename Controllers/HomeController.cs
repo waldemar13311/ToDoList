2c201dca955ce0bda7f09fb430d92e60f82e10c1
waldemar13311 <waldemar13311@gmail.com>
2021-11-02 00:09:01 +0300

    Started work on list function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,16 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            ViewBag.Title = "Hello, world";
-            
+            var user = _userManager.GetUserName(User);
+
+            ViewBag.Title = $"Hello, {user}";
+
+
             return View();
         }
     }
